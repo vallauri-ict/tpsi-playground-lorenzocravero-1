4a159aaedbf608f5cf1b869196bced525c99d445
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-12-15 12:46:09 +0100

    crud server quasi terminato

--- Express/Ese_02 Crud server/server.ts
+++ Express/Ese_02 Crud server/server.ts
@@ -1,43 +1,39 @@
-"use strict"
+"use strict";
 
 import express from "express";
-import * as _http from 'http'
-import * as _url from 'url'
-import * as _fs from 'fs'
-import * as _mongodb from "mongodb"
+import * as _http from "http";
+import * as _url from "url";
+import * as _fs from "fs";
+import * as _mongodb from "mongodb";
 import * as _bodyparser from "body-parser";
 import { inherits } from "util";
 
-
-const app =  express();
+const app = express();
 const mongoClient = _mongodb.MongoClient;
 //const CONNECTIONSTRING = "mongodb://127.0.0.1:27017";
-const CONNSTRING = "mongodb://lorenzocravero:lollo@cluster0-shard-00-00.iwwbt.mongodb.net:27017,cluster0-shard-00-01.iwwbt.mongodb.net:27017,cluster0-shard-00-02.iwwbt.mongodb.net:27017/test?replicaSet=atlas-gt4d36-shard-0&ssl=true&authSource=admin";
+const CONNSTRING =
+  "mongodb://lorenzocravero:lollo@cluster0-shard-00-00.iwwbt.mongodb.net:27017,cluster0-shard-00-01.iwwbt.mongodb.net:27017,cluster0-shard-00-02.iwwbt.mongodb.net:27017/test?replicaSet=atlas-gt4d36-shard-0&ssl=true&authSource=admin";
 const DBNAME = "unicorns";
-const PORT = 1337; 
-
+const PORT = 1337;
 
 const server = _http.createServer(app);
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
   console.log("Server in ascolto sulla porta: " + PORT);
   init();
 });
 
 //creiamo una variabile globale per contenere una pagina di errore
-let paginaErrore  = "";
+let paginaErrore = "";
 
-function init(){
-  _fs.readFile("./static/error.html",(err,data)=>{
-    if(!err)
-    {
+function init() {
+  _fs.readFile("./static/error.html", (err, data) => {
+    if (!err) {
       paginaErrore = data.toString();
-    }
-    else
-    {
+    } else {
       paginaErrore = "<h2>Risorsa non trovata</h2>";
     }
-  })
+  });
 }
 
 /* **************************************************** */
@@ -45,57 +41,90 @@ function init(){
 /* **************************************************** */
 
 // 1)log
-app.use("/",(req,res,next)=>{
+app.use("/", (req, res, next) => {
   console.log("=>" + req.method + ":" + req.originalUrl);
   next();
-})
+});
 
 // 2)static route
 //va nella cartella static a cercare se trova un file col nome della risorsa,
 //se lo trova li restituisce, altrimenti fa next ed il controllo passa all'app.use successiva
-app.use("/",express.static("./static"));
+app.use("/", express.static("./static"));
 
 // 3)route di lettura dei parametri POST
-app.use("/",_bodyparser.json());
-app.use("/",_bodyparser.urlencoded({"extended" : true}));
+app.use("/", _bodyparser.json());
+app.use("/", _bodyparser.urlencoded({ extended: true }));
 
 // 4)log dei parametri
-app.use("/",(req,res,next)=>{
-  if(Object.keys(req.query).length > 0)
-  {
-    console.log("      Parametri GET : " , req.query);
+app.use("/", (req, res, next) => {
+  if (Object.keys(req.query).length > 0) {
+    console.log("      Parametri GET : ", req.query);
   }
-  if(Object.keys(req.body).length > 0)
-  {
-    console.log("      Parametri BODY : " , req.body);
+  if (Object.keys(req.body).length > 0) {
+    console.log("      Parametri BODY : ", req.body);
   }
   next();
-})
-
+});
 
 /* **************************************************** */
 /// Elenco delle route di risposta al client
 /* **************************************************** */
 
-//apriamo la connessione per tutte le query
-app.use("/",function(req,res,next){
-  mongoClient.connect(CONNSTRING,function(err,client){
-    if(err)
-    {
+//middleware di aprtura della connessione
+app.use("/", function (req, res, next) {
+  mongoClient.connect(CONNSTRING, function (err, client) {
+    if (err) {
       res.status(503).send("Errore di connessione al database");
-    }
-    else
-    {
+    } else {
       req["client"] = client;
       next();
     }
-  })
+  });
 });
 
+
+
+
 //modello di query con express con parametri come json
-app.get("/api/getCollections",(req,res,next)=>{
+//lettura delle collezioni del database
+//va per forza messo prima perchè se no rispondono prima gli altri
+//e qui non entrano mai
+app.get("/api/getCollections", (req, res, next) => {
   let db = req["client"].db(DBNAME);
-    let request = db.listCollections().toArray();
+  let request = db.listCollections().toArray();
+  request.then(function(data){
+    res.send(data);
+  });
+
+  request.catch(function(err){
+    res.status(500).send("Errore nell'esecuzione della query" + err);
+  });
+
+  request.finally(function(){
+    req["client"].close();
+  });
+});
+
+
+
+//middleware per intercettare i parametri
+let currentCollection = "";
+let id = "";
+
+app.use("/api/:collection/:id?", (req, res, next) => {
+  currentCollection = req.params.collection;
+  id = req.params.id;
+  next();
+});
+
+
+//middleware specifico per tutte le chiamate get
+app.get("/api/*", (req, res, next) => {
+  let db = req["client"].db(DBNAME);
+  let collection = db.collection(currentCollection);
+  if (!id) 
+  {
+    let request = collection.find().toArray();
     request.then(function(data){
       res.send(data);
     });
@@ -108,28 +137,41 @@ app.get("/api/getCollections",(req,res,next)=>{
       req["client"].close();
     });
   }
-);
+  else
+  {
+    let oId = new _mongodb.ObjectId(id);
+
+    let request = collection.find({"_id" : oId}).toArray();
+    request.then(function(data){
+      res.send(data);
+    });
+
+    request.catch(function(err){
+      res.status(500).send("Errore nell'esecuzione della query" + err);
+    });
+
+    request.finally(function(){
+      req["client"].close();
+    });
+  }
+});
+
 
 
 /* **************************************************** */
 /// Elenco delle route di gestione dell'errore
 /* **************************************************** */
 
-app.use("/",(req,res,next) =>{
+app.use("/", (req, res, next) => {
   res.status(404);
-  if(req.originalUrl.startsWith("/api/"))
-  {
+  if (req.originalUrl.startsWith("/api/")) {
     res.send("Risorsa non trovata");
-  }
-  else
-  {
+  } else {
     res.send(paginaErrore);
   }
-})
+});
 
 //route per la gestione dell'errore
-app.use(function(err,req,res,next){
+app.use(function (err, req, res, next) {
   console.log("Errore lato server " + err.message);
-})
-
-
+});
--- Express/Ese_02 Crud server/static/index.js
+++ Express/Ese_02 Crud server/static/index.js
@@ -2,13 +2,17 @@
 
 $(document).ready(function() {
 
-    let divIntestazione = $("#divIntestazione")
-    let divCollections = $("#divCollections")
-    let table = $("#mainTable")
-    let divDettagli = $("#divDettagli")
+    let divIntestazione = $("#divIntestazione");
+    let divCollections = $("#divCollections");
+    let divFilters = $(".card").eq(0);
+    let table = $("#mainTable");
+    let divDettagli = $("#divDettagli");
     let currentCollection = "";
+    let tbody = $("table").children("tbody");
 
-    let request = inviaRichiesta("get", "api/getCollections");
+    divFilters.hide();
+
+    let request = inviaRichiesta("get","/api/getCollections");
     request.fail(errore)
     request.done(function(collections) {
 	  console.log(collections);
@@ -27,13 +31,69 @@ $(document).ready(function() {
       label.remove();
     })
 
+    //non posso fare input.click perchè all'onload non è stato ancora creato
     divCollections.on("click","input[type=radio]",function(){
-        alert("funziona");
-    });
+        tbody.empty();
+        currentCollection = $(this).val();
+        let request = inviaRichiesta("GET","/api/"+currentCollection);
+        request.fail(errore);
+        request.done(function(data){
+            console.log(data);
+            divIntestazione.find("strong").eq(0).text(currentCollection);
+            divIntestazione.find("strong").eq(1).text(data.length);
+            if(currentCollection == "unicorns")
+            {
+                divFilters.slideDown();
+            }
+            else
+            {
+                divFilters.slideUp();
+            }
 
+            //popolamento tabella
+            for (const item of data) 
+            {
+                let tr = $("<tr>");
+                tr.appendTo(tbody);
 
-   
-   
-   
-   
+                let td = $("<td>");
+                td.appendTo(tr);
+                td.prop("width","30%");
+                td.text(item["_id"]);
+                td.prop("id",item["_id"]);
+                td.on("click",visualDettagli);
+
+                td = $("<td>");
+                td.appendTo(tr);
+                td.prop("width","40%");
+                td.text(item["name"]);
+                td.prop("id",item["_id"]);
+                td.on("click",visualDettagli);
+
+                td = $("<td>");
+                td.appendTo(tr);
+                td.prop("width","30%");
+                //creiamo 3 div e basta perchè ci pensa il css
+                for (let i = 0; i < 3; i++) 
+                {
+                    $("<div>").appendTo(td); 
+                }
+            }
+        })
+    }); 
+    
+    function visualDettagli(){
+        let currentId = $(this).prop("id");
+        let request = inviaRichiesta("GET","/api/"+currentCollection+"/"+currentId);
+        request.fail(errore);
+        request.done(function(data){
+            console.log(data);
+            let content = "";
+            for (const key in data[0]) 
+            {
+                content += "<strong>"+key+": </strong>" + data[0][key] + "<br>";
+                divDettagli.html(content);
+            }
+        })
+    }
 });
